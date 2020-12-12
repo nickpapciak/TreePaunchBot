0b920aae17bdba9e0368820d6e75012f833c1e51
Nick Papciak <nicholaspapciak@gmail.com>
2020-12-12 05:49:07 +0000

    renamed Data to data in a spot I missed

--- cogs/Todo.py
+++ cogs/Todo.py
@@ -12,79 +12,81 @@ from main import TreePaunchBot
 FILEPATH = "data/todos.json"
 
 # creates a source class for the menus module
+
+
 class TodoMenuSource(menus.ListPageSource):
     def __init__(self, data):
         super().__init__(data, per_page=6)
 
     async def format_page(self, menu, entries):
         offset = (menu.current_page * self.per_page) + 1
-        # sends the correct page 
-        embed_text = '\n'.join(f'{index}. {item}' for index, item in enumerate(entries, start=offset))
-        return discord.Embed(description = embed_text, colour =  TreePaunchBot.color)
+        # sends the correct page
+        embed_text = '\n'.join(
+            f'{index}. {item}' for index, item in enumerate(entries, start=offset))
+        return discord.Embed(description=embed_text, colour=TreePaunchBot.color)
+
 
 class Todo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def todo_logger(self): 
-      with open(FILEPATH , "r") as f: 
-        dict = json.load(f)
+    async def todo_logger(self):
+        with open(FILEPATH, "r") as f:
+            dict = json.load(f)
 
-        url_key = await self.bot.session.post("https://mystb.in/documents", data = json.dumps(dict, indent=6))
-        url_key = await url_key.json()
+            url_key = await self.bot.session.post("https://mystb.in/documents", data=json.dumps(dict, indent=6))
+            url_key = await url_key.json()
 
-      channel = self.bot.get_channel(735989735792705616)
-      todo_embed = discord.Embed(
-        title = datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        url = f"https://mystb.in/{url_key['key']}.json", 
-        colour =  self.bot.color)
-      await channel.send(embed=todo_embed)
+        channel = self.bot.get_channel(735989735792705616)
+        todo_embed = discord.Embed(
+            title=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            url=f"https://mystb.in/{url_key['key']}.json",
+            colour=self.bot.color)
+        await channel.send(embed=todo_embed)
 
-    @commands.group(aliases = ['t'], case_insensitive=True)
+    @commands.group(aliases=['t'], case_insensitive=True)
     async def todo(self, ctx):
         """TODO command that allows a user to store tasks"""
 
-
-    @todo.command(name = "add", aliases = ["create", "start"])
-    async def todo_add_cmd(self, ctx, *, todo=None): 
-      if todo is None: 
-        added_todo = "No todo was specified."
-      else:
-        added_todo = todo_add(str(ctx.author.id), str(todo))
-      todo_embed = discord.Embed(
-        description = added_todo, 
-        colour =  self.bot.color)
-      await ctx.send(embed=todo_embed)
-      await self.todo_logger()
-
-    @todo.command(name = "view", aliases = ["v", "see", "check", "list"])
-    async def todo_view_cmd(self, ctx): 
-      todo_list = todo_view(str(ctx.author.id)) # generates a list of todo's
-
-      if todo_list == []: #if the list is empty
+    @todo.command(name="add", aliases=["create", "start"])
+    async def todo_add_cmd(self, ctx, *, todo=None):
+        if todo is None:
+            added_todo = "No todo was specified."
+        else:
+            added_todo = todo_add(str(ctx.author.id), str(todo))
         todo_embed = discord.Embed(
-          description = "You have nothing in your todo list!", 
-          colour =  self.bot.color)
+            description=added_todo,
+            colour=self.bot.color)
         await ctx.send(embed=todo_embed)
-      else: 
-        pages = menus.MenuPages(source=TodoMenuSource(todo_list), clear_reactions_after=True) # creates a menu
-        await pages.start(ctx)
         await self.todo_logger()
 
-    @todo.command(name = "delete", aliases = ["del", "remove", "end", "finish"])
-    async def todo_delete_cmd(self, ctx, index=None): 
-      if index is None: 
-        deleted_todo = "No index specified."
-      else: 
-        deleted_todo = todo_delete(str(ctx.author.id), index)
-      todo_embed = discord.Embed(
-        description = deleted_todo, 
-        colour =  self.bot.color)
-      await ctx.send(embed=todo_embed)
-      await self.todo_logger()
+    @todo.command(name="view", aliases=["v", "see", "check", "list"])
+    async def todo_view_cmd(self, ctx):
+        todo_list = todo_view(str(ctx.author.id))  # generates a list of todo's
+
+        if todo_list == []:  # if the list is empty
+            todo_embed = discord.Embed(
+                description="You have nothing in your todo list!",
+                colour=self.bot.color)
+            await ctx.send(embed=todo_embed)
+        else:
+            pages = menus.MenuPages(source=TodoMenuSource(
+                todo_list), clear_reactions_after=True)  # creates a menu
+            await pages.start(ctx)
+            await self.todo_logger()
+
+    @todo.command(name="delete", aliases=["del", "remove", "end", "finish"])
+    async def todo_delete_cmd(self, ctx, index=None):
+        if index is None:
+            deleted_todo = "No index specified."
+        else:
+            deleted_todo = todo_delete(str(ctx.author.id), index)
+        todo_embed = discord.Embed(
+            description=deleted_todo,
+            colour=self.bot.color)
+        await ctx.send(embed=todo_embed)
+        await self.todo_logger()
+
 
 def setup(bot):
     bot.add_cog(Todo(bot))
-
-
-
--- util/todo_util.py
+++ util/todo_util.py
@@ -1,53 +1,60 @@
 import json
-FILEPATH = "Data/todos.json"
+FILEPATH = "data/todos.json"
 
-def todo_add(user, todo_text): 
-    with open(FILEPATH , "r+") as f: 
-        todo_dict = json.load(f) # loads the todo's as a dict
-    
-        if user in todo_dict.keys():  # goes through each user 
-            todo_dict[user].append(todo_text) # appends the new item into the dict
-        else: 
-            todo_dict[user] = [todo_text] # if user has never been cached it creates a new user with a new todo list
 
-        f.seek(0) # moves cursor to beginning of file
-        f.truncate(0) # deletes file contents 
-        json.dump(todo_dict, f, indent=6) # loads the new dict into the file 
+def todo_add(user, todo_text):
+    with open(FILEPATH, "r+") as f:
+        todo_dict = json.load(f)  # loads the todo's as a dict
+
+        if user in todo_dict.keys():  # goes through each user
+            # appends the new item into the dict
+            todo_dict[user].append(todo_text)
+        else:
+            # if user has never been cached it creates a new user with a new todo list
+            todo_dict[user] = [todo_text]
+
+        f.seek(0)  # moves cursor to beginning of file
+        f.truncate(0)  # deletes file contents
+        json.dump(todo_dict, f, indent=6)  # loads the new dict into the file
 
         return f"Added **{todo_text}** to your todo list!"
 
-def todo_view(user): 
-    with open(FILEPATH , "r") as f: 
-        todo_dict = json.load(f) # loads the file into a dict
+
+def todo_view(user):
+    with open(FILEPATH, "r") as f:
+        todo_dict = json.load(f)  # loads the file into a dict
+
+    # if user is in the keys and not empty []
+    if user in todo_dict.keys() and todo_dict[user]:
+        return todo_dict[user]  # returns the list
+
+    return []  # returns empty list only if the user doesn't exist/doesn't have todo's
 
 
-    if user in todo_dict.keys() and todo_dict[user]: # if user is in the keys and not empty []
-      return todo_dict[user] # returns the list
+def todo_delete(user, index):
+    with open(FILEPATH, "r+") as f:
+        todo_dict = json.load(f)  # loads the file into a dict
 
-    return [] # returns empty list only if the user doesn't exist/doesn't have todo's
-
-def todo_delete(user, index): 
-    with open(FILEPATH , "r+") as f: 
-        todo_dict = json.load(f) # loads the file into a dict
-        
-        # catches type errors 
-        try: 
+        # catches type errors
+        try:
             index = int(index)
-        except: 
+        except:
             return "Index not in proper format. Make sure index is positive non-zero integer."
 
+        # if user is in the keys and not empty []
+        if user in todo_dict.keys() and todo_dict[user]:
 
-        if user in todo_dict.keys() and todo_dict[user]: # if user is in the keys and not empty []
-
-            if index > len(todo_dict[user]) or index <=0: # if the index is outside the bounds
+            # if the index is outside the bounds
+            if index > len(todo_dict[user]) or index <= 0:
                 return f"Could not find item at position **{index}** in todo list"
-            else: 
-                deleted_item = todo_dict[user].pop(index-1) # stores the deleted item and removes it 
-                f.seek(0) # moves cursor to beginning of file
-                f.truncate(0) # deletes file contents 
-                json.dump(todo_dict, f, indent=6) # dumps the new dictionary without the deleted item
+            else:
+                # stores the deleted item and removes it
+                deleted_item = todo_dict[user].pop(index-1)
+                f.seek(0)  # moves cursor to beginning of file
+                f.truncate(0)  # deletes file contents
+                # dumps the new dictionary without the deleted item
+                json.dump(todo_dict, f, indent=6)
                 return f"Deleted **{index}.) {deleted_item}** from your todo list!"
-                
+
         else:  # if the user doesn't exist/has no todo's
             return "You have nothing in your todo list"
-
